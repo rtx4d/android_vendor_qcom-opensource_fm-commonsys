a6697b1348aa84794c64a7a0d6722488215e1044
LuK1337 <priv.luk@gmail.com>
2018-03-31 19:05:12 +0200

    fm_hci/qcomfm_jni/helium: Exclude from OSS builds
    * These libraries depend on closed source
      headers which we don't include in OSS builds.
    
    Change-Id: I8fc82ff468e1f58eebd4fe0ab4b3f832cd7e2bab

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,3 +1,4 @@
+ifneq ($(QCPATH),)
 ifeq ($(BOARD_HAVE_QCOM_FM),true)
 ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
 LOCAL_PATH := $(call my-dir)
@@ -32,3 +33,4 @@ include $(BUILD_SHARED_LIBRARY)
 
 endif # is-vendor-board-platform
 endif # BOARD_HAVE_QCOM_FM
+endif
